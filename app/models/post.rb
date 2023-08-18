ff07bdc60211aca13cfbd177190b17f4e9b7a115
mrcbq <marcobaquero@gmail.com>
2023-08-17 22:22:29 -0500

    Add method that updates the posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-  # after_create :update_posts_count
-  # after_destroy :update_posts_count
+  after_create :update_posts_count
+  after_destroy :update_posts_count
 
-  # def update_posts_count
-  #   author.update(posts_count: author.posts.count)
-  # end
+  def update_posts_count
+    author.update(posts_count: author.posts.count)
+  end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
