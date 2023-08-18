102c2f60929bb64b5b4c8537fac4534738855cbb
mrcbq <marcobaquero@gmail.com>
2023-08-18 10:18:55 -0500

    Add validations to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   after_create :update_author_posts_count
   after_destroy :update_author_posts_count
 
