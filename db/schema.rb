095b337a651b0412b5a6e40d4761b7cc4f279cd6
mrcbq <marcobaquero@gmail.com>
2023-08-17 22:16:17 -0500

    Run Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_17_000454) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_18_003152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_17_000454) do
     t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count"
   end
 
 end
