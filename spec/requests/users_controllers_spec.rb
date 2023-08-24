d50e3212aca3815e33b480cda79e53c5714fcada
mrcbq <marcobaquero@gmail.com>
2023-08-23 22:38:46 -0500

    Add test logic for users

--- spec/requests/users_controllers_spec.rb
+++ spec/requests/users_controllers_spec.rb
@@ -1,10 +1,37 @@
-require 'rails_helper'
+require './spec/rails_helper'
 
-RSpec.describe "UsersControllers", type: :request do
-  describe "GET /users_controllers" do
-    it "works! (now write some real specs)" do
-      get users_controllers_path
-      expect(response).to have_http_status(200)
+RSpec.describe UsersController, type: :request do
+  describe 'GET #show' do
+    it 'renders a successful response' do
+      get '/users/show'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/show'
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the correct placeholder in the response body' do
+      get '/users/show'
+      expect(response.body).to include('Here is the description and posts for a given user')
+    end
+  end
+
+  describe 'GET #index' do
+    it 'renders a successful response' do
+      get '/users'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the index template' do
+      get '/users'
+      expect(response).to render_template('index')
+    end
+
+    it 'renders the correct placeholder in the response body' do
+      get '/users'
+      expect(response.body).to include('Here is a list of all users')
     end
   end
 end
