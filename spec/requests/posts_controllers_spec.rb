33c0bbd518b3c5d7e4fe5e6c67eb9f768b26c1b3
mrcbq <marcobaquero@gmail.com>
2023-08-23 22:41:25 -0500

    Add test logic for Posts

--- spec/requests/posts_controllers_spec.rb
+++ spec/requests/posts_controllers_spec.rb
@@ -1,10 +1,37 @@
-require 'rails_helper'
+require './spec/rails_helper'
 
-RSpec.describe "PostsControllers", type: :request do
-  describe "GET /posts_controllers" do
-    it "works! (now write some real specs)" do
-      get posts_controllers_path
-      expect(response).to have_http_status(200)
+RSpec.describe PostsController, type: :request do
+  describe 'GET #show' do
+    it 'renders a successful response' do
+      get '/users/show/posts/show'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/show/posts/show'
+      expect(response).to render_template('show')
+    end
+
+    it 'renders the correct placeholder in the response body' do
+      get '/users/show/posts/show'
+      expect(response.body).to include('Here is the description and comments of a given post')
+    end
+  end
+
+  describe 'GET #index' do
+    it 'renders a successful response' do
+      get '/users/show/posts'
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the show template' do
+      get '/users/show/posts'
+      expect(response).to render_template('index')
+    end
+
+    it 'renders the correct placeholder in the response body' do
+      get '/users/show/posts'
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 end
