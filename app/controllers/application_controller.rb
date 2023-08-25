537ac5ed240799ea1f8fb95a5a8b3f8d335c2ca7
mrcbq <marcobaquero@gmail.com>
2023-08-25 16:38:16 -0500

    Add logic to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user = @user || User.first
+  end
+
+  def add_like
+    @like = Like.new(user: @user, post: @post)
+    return unless @like.save
+
+    print 'asgas'
+  end
+
+  helper_method :current_user
 end
