fafa7dea1e3a91bf93f1eaeb1a68aa80c065def3
mrcbq <marcobaquero@gmail.com>
2023-08-29 19:30:02 -0500

    Update post controllers specs to the new routes

--- spec/requests/posts_controllers_spec.rb
+++ spec/requests/posts_controllers_spec.rb
@@ -1,37 +1,40 @@
 require './spec/rails_helper'
 
 RSpec.describe PostsController, type: :request do
+  let!(:user) { User.create!( name: 'John', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Tester') }
+  let!(:post) { Post.create!(author: user, title: 'Post test', text: 'Description test') }
+
   describe 'GET #show' do
     it 'renders a successful response' do
-      get '/users/show/posts/show'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response.status).to eq(200)
     end
 
     it 'renders the show template' do
-      get '/users/show/posts/show'
+      get "/users/#{user.id}/posts/#{post.id}"
       expect(response).to render_template('show')
     end
 
-    it 'renders the correct placeholder in the response body' do
-      get '/users/show/posts/show'
-      expect(response.body).to include('Here is the description and comments of a given post')
+    it 'assigns the correct post' do
+      get "/users/#{user.id}/posts/#{post.id}"
+      expect(assigns(:post)).to eq(post)
     end
   end
 
   describe 'GET #index' do
     it 'renders a successful response' do
-      get '/users/show/posts'
+      get "/users/#{user.id}/posts"
       expect(response.status).to eq(200)
     end
 
-    it 'renders the show template' do
-      get '/users/show/posts'
+    it 'renders the index template' do
+      get "/users/#{user.id}/posts"
       expect(response).to render_template('index')
     end
 
-    it 'renders the correct placeholder in the response body' do
-      get '/users/show/posts'
-      expect(response.body).to include('Here is a list of posts for a given user')
+    it 'assigns the correct posts' do
+      get "/users/#{user.id}/posts"
+      expect(assigns(:posts)).to eq([post])
     end
   end
 end
