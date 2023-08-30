1482ebf14e8bd7828e31ae1a7e185b09798a39e9
Abraham Christopher <68823331+Cabraham1@users.noreply.github.com>
2023-08-30 19:33:09 +0100

    Update user_posts_index_spec.rb

--- spec/integration/user_posts_index_spec.rb
+++ spec/integration/user_posts_index_spec.rb
@@ -7,19 +7,19 @@ RSpec.describe 'User Post Index Page', type: :feature do
     5.times { |i| user.posts.create(title: "Post #{i + 1}", text: "text #{i + 1}") }
   end
 
-  # it 'displays the user\'s profile picture' do
-  #   visit user_posts_path(user)
-  #   expect(page).to have_css("img[src*='photo.jpg']")
-  # end
+  it 'displays the user\'s profile picture' do
+    visit user_posts_path(user)
+    expect(page).to have_css("img[src*='photo.jpg']")
+  end
 
-  # it 'displays the user\'s username' do
-  #   visit user_posts_path(user)
-  #   expect(page).to have_content('User')
-  # end
+  it 'displays the user\'s username' do
+    visit user_posts_path(user)
+    expect(page).to have_content('User')
+  end
 
-  # it 'displays the number of posts the user has written' do
-  #   visit user_posts_path(user)
-  #   expect(page).to have_content('Number of posts: 5')
-  # end
+  it 'displays the number of posts the user has written' do
+    visit user_posts_path(user)
+    expect(page).to have_content('Number of posts: 5')
+  end
 
 end
