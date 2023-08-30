b4decd2a1264c9aeb2071653e62990d51fec1adb
Abraham Christopher <68823331+Cabraham1@users.noreply.github.com>
2023-08-30 19:34:07 +0100

    Update user_posts_show_spec.rb

--- spec/integration/user_posts_show_spec.rb
+++ spec/integration/user_posts_show_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe 'Post Show Page', type: :feature do
   let!(:post) { user.posts.create(title: 'Post Title', text: 'Post text') }
   let!(:comment) { post.comments.create(author: user, text: 'Comment text') }
 
-  # it 'displays the post\'s title' do
-  #   visit user_posts_path(user)
-  #   expect(page).to have_content('Post text')
-  # end
+  it 'displays the post\'s title' do
+    visit user_posts_path(user)
+    expect(page).to have_content('Post text')
+  end
   
-  # it 'displays who wrote the post' do
-  #   visit user_post_path(user, post)
-  #   expect(page).to have_content('by User')
-  # end
+  it 'displays who wrote the post' do
+    visit user_post_path(user, post)
+    expect(page).to have_content('by User')
+  end
   
-  # it 'displays the number of comments the post has' do
-  #   visit user_post_path(user, post)
-  #   expect(page).to have_content('Comments: 1')
-  # end
+  it 'displays the number of comments the post has' do
+    visit user_post_path(user, post)
+    expect(page).to have_content('Comments: 1')
+  end
 
 end
