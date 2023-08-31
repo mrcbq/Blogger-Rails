6db7dfab6232530d574bff5a32189d2b620e2c91
mrcbq <marcobaquero@gmail.com>
2023-08-31 15:34:41 -0500

    Add a comprobation if the user is logged

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_user
+  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :set_user
 
   def index
     @page_title = 'Post Index Page'
