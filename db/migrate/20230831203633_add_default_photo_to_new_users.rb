a6c6d23d84b3646b2ff46b1b27f0ee9d44b7cf91
mrcbq <marcobaquero@gmail.com>
2023-08-31 15:41:45 -0500

    Add default photo for new Users, run migration

--- db/migrate/20230831203633_add_default_photo_to_new_users.rb
+++ db/migrate/20230831203633_add_default_photo_to_new_users.rb
@@ -1,4 +1,5 @@
 class AddDefaultPhotoToNewUsers < ActiveRecord::Migration[7.0]
   def change
+    change_column :users, :photo, :string, default: "https://randomuser.me/api/portraits/men/10.jpg"
   end
 end
