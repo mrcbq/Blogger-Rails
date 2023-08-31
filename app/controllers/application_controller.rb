774ac595af329569887868215747f06d90c36d57
mrcbq <marcobaquero@gmail.com>
2023-08-30 23:03:25 -0500

    Remove current_user method in ApplicationController, devise will provide us with one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user = @user || User.first
-  end
+  # def current_user
+  #   @current_user = @user || User.first
+  # end
 
   def add_like
     @like = Like.new(user: @user, post: @post)
