b86a8ce950928b2dcddf4cb4a1af7a93771edf1b
mrcbq <marcobaquero@gmail.com>
2023-08-31 17:53:17 -0500

    Remove default photo in database and create in model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_31_203633) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_225016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_31_203633) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "bio"
-    t.string "photo", default: "https://randomuser.me/api/portraits/men/10.jpg"
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "posts_count"
